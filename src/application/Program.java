6ae2f22f8ff6445197315bb86d73d4b3d7b1d19c
Samuel Felipe <samuelfelipemz@outlook.com>
2025-12-27 08:52:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +9,10 @@ public class Program {
 
         Department  obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new java.util.Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 
 }
